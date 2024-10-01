204626144d411d843da6ffbec1860416bba1549f
Jee-Yu Yang <jeeyu96@gmail.com>
2024-10-01 17:36:39 +0200

    new settings for tailwindcss and nativewin

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from "expo-router";
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hey this is the WifeyHub! </Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text> Hey this is the WHub! </Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue"}}> Go to Profile </Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
